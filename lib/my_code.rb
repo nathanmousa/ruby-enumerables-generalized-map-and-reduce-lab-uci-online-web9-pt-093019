674e895f53563f0701415051fa8cf65d61297331
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-02 00:02:48 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(yield(source_array[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(source_array,starting_point = nil)
+  if starting_point
+  origin = starting_point
+  i = 0
+else
+  origin = source_array[0]
+  i = 1
+end
+while i < source_array.length
+  origin = yield(origin, source_array[i])
+  i += 1
+end
+origin
+end
